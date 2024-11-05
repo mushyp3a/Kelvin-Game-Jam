77f675e578a79584471613ab01b0f2c400a1109c
Mackenzie <m.o.trafford@gmail.com>
2024-11-05 16:07:37 +0000

    update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,11 +1,3 @@
 import json
 
-x = {"test":{"a":1,"b":2}, "test2":2}
-
-with open("data.json",'w') as f:
-    json.dump(x,f)
-
-with open('data.json','r') as f:
-    y=json.load(f)
-
-print(y["test"])
+core = ['Ae', 'Qu', 'Du', 'Nb', 'Cy']
